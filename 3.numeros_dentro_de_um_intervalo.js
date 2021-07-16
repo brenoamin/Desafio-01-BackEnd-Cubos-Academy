f63ddb12c27fa9a8cae097980f85a64bb3bd5754
Breno Amin <brenomatos125@gmail.com>
2021-07-16 17:30:43 -0300

    03. Numeros dentro de um intervalo

---
+++ 3.numeros_dentro_de_um_intervalo.js
@@ -0,0 +1,6 @@
+const numero = -1
+const limiteInferior = 0
+const limiteSuperior = 10
+function solucao(numero, limiteInferior, limiteSuperior) {
+    console.log((numero >= limiteInferior && numero <= limiteSuperior) ? "PERTENCE" : "NAO PERTENCE")
+}
