7bb1135ec5061b947c7fde86d3d1e50c1376ee1a
Breno Amin <brenomatos125@gmail.com>
2021-07-16 17:32:06 -0300

    06.Caçula dos Adultos

---
+++ 6.caçula_dos_adultos.js
@@ -0,0 +1,13 @@
+const lista = [18, 0, 5]
+function solucao(lista) {
+    const listaFiltrado = lista.filter((x) => x >= 18)
+    if (listaFiltrado.length < 1) {
+        console.log("CRESCA E APARECA")
+    }
+    else {
+        const menor = listaFiltrado.reduce((accumulator, currentValue) => currentValue < accumulator ? currentValue : accumulator)
+        console.log(menor)
+    }
+
+}
+solucao(lista)
