9b63a4508a1e1705b81810fcddce30acfee39bd7
Breno Amin <brenomatos125@gmail.com>
2021-07-16 17:30:10 -0300

    Media Aritmetica

---
+++ 2.media_aritmetica.js
@@ -0,0 +1,12 @@
+const lista = [2, 3, 4]
+
+function solucao(lista) {
+    const soma = lista.reduce((accumulator, currentValue) => {
+        accumulator = accumulator + currentValue;
+        return accumulator
+    })
+    const media = soma / lista.length
+    console.log(media)
+
+}
+solucao(lista)
